782131f8c289f6fd264b82b2605fa3513c850d4b
Katsuma Narisawa <face.the.music721@gmail.com>
2018-07-21 14:05:24 +0900

    Implement CosineMeasure

--- sample.py
+++ sample.py
@@ -1,11 +1,21 @@
 # coding: utf8
 
 from simstring.feature_extractor.ngram import NgramFeatureExtractor
-feature_extractor = NgramFeatureExtractor(2);
+feature_extractor = NgramFeatureExtractor(2)
 print feature_extractor.features(u"あいうえお")
 
 from simstring.measure.cosine import CosineMeasure
+measure = CosineMeasure()
 
+query_size = 10
+alpha = 0.8
+y_size = 5
+X = 'aiueo'
+Y = 'aiueo'
+print measure.min_feature_size(query_size, alpha)
+print measure.max_feature_size(query_size, alpha)
+print measure.minimum_common_feature_count(query_size, y_size, alpha)
+print measure.similarity(X, Y)
 
 from simstring.database.dict import DictDatabase
 from simstring.searcher import Searcher
--- simstring/measure/base.py
+++ simstring/measure/base.py
@@ -1,3 +1,12 @@
 class BaseMeasure:
-    def similarity(self, a, b):
-        return 1.0
+    def min_feature_size(self, _query_size, _alpha):
+        raise 'Not Implemented'
+
+    def max_feature_size(self, _query_size, _alpha):
+        raise 'Not Implemented'
+
+    def minimum_common_feature_count(self, _query_size, _y_size, _alpha):
+        raise 'Not Implemented'
+
+    def similarity(self, X, Y):
+        raise 'Not Implemented'
