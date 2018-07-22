cecb100c97c7e67cd70236d7044cb4e3ad47a776
Katsuma Narisawa <face.the.music721@gmail.com>
2018-07-22 15:16:34 +0900

    Implement WordNgramFeatureExtractor

--- simstring/feature_extractor/base.py
+++ simstring/feature_extractor/base.py
@@ -1,3 +1,6 @@
 class BaseFeatureExtractor:
     def features(self, _string):
         raise NotImplementedError()
+
+    def _each_cons(self, xs, n):
+        return [xs[i:i+n] for i in range(len(xs)-n+1)]
