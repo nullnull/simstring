640a091ff4126316971d7ea8ba6137412ea73b4e
Katsuma Narisawa <face.the.music721@gmail.com>
2018-07-22 13:15:09 +0900

    Implement MongoDatabase

--- sample.py
+++ sample.py
@@ -1,18 +1,20 @@
-# coding: utf8
+# coding: utf-8
 
 from simstring.feature_extractor.ngram import NgramFeatureExtractor
 from simstring.measure.cosine import CosineMeasure
+from simstring.database.mongo import MongoDatabase
 from simstring.database.dict import DictDatabase
 from simstring.searcher import Searcher
 
 string = "aiueo"
 
-db = DictDatabase(NgramFeatureExtractor(2))
+db = MongoDatabase(NgramFeatureExtractor(2))
+db.reset_collection()
 db.add(string)
 db.add('aiueoo')
 db.add('aiueooo')
+print(db.all_documents())
 
 searcher = Searcher(db, CosineMeasure())
 xs = searcher.search(string, 1.0)
-
 print(xs)
--- simstring/database/dict.py
+++ simstring/database/dict.py
@@ -20,11 +20,14 @@ class DictDatabase(BaseDatabase):
         for feature in features:
             self.feature_set_size_and_feature_to_string_map[size][feature].add(string)
 
+    def all(self):
+        return self.strings
+
+    def lookup_strings_by_feature_set_size_and_feature(self, size, feature):
+        return self.feature_set_size_and_feature_to_string_map[size][feature]
+
     def min_feature_size(self):
         return min(self.feature_set_size_to_string_map.keys())
 
     def max_feature_size(self):
         return max(self.feature_set_size_to_string_map.keys())
-
-    def lookup_strings_by_feature_set_size_and_feature(self, size, feature):
-        return self.feature_set_size_and_feature_to_string_map[size][feature]
