db70f2a6e379eca865205605f6d13629dbf4433a
Katsuma Narisawa <face.the.music721@gmail.com>
2018-07-22 16:12:39 +0900

    Implement other measures

---
+++ simstring/measure/dice.py
@@ -0,0 +1,15 @@
+import math
+from .base import BaseMeasure
+
+class DiceMeasure(BaseMeasure):
+    def min_feature_size(self, query_size, alpha):
+        return int(math.ceil(alpha * 1.0 / (2 - alpha) * query_size))
+
+    def max_feature_size(self, query_size, alpha):
+        return int(math.floor((2 - alpha) * query_size * 1.0 / alpha))
+
+    def minimum_common_feature_count(self, query_size, y_size, alpha):
+        return int(math.ceil(0.5 * alpha * query_size * y_size))
+
+    def similarity(self, X, Y):
+        return len(set(X) & set(Y)) * 2.0 / (len(X) + len(Y))
