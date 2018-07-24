41c179c897f54ec036e0c18138775ce85cde86f6
Katsuma Narisawa <face.the.music721@gmail.com>
2018-07-24 10:22:02 +0900

    Add badges (#6)
    Add badges

--- simstring/feature_extractor/base.py
+++ simstring/feature_extractor/base.py
@@ -4,3 +4,6 @@ class BaseFeatureExtractor:
 
     def _each_cons(self, xs, n):
         return [xs[i:i+n] for i in range(len(xs)-n+1)]
+
+    def _words_ngram(self, words, n, SENTINAL_CHAR):
+        return [tuple(x) for x in self._each_cons([SENTINAL_CHAR] + words + [SENTINAL_CHAR], n)]
--- simstring/feature_extractor/word_ngram.py
+++ simstring/feature_extractor/word_ngram.py
@@ -11,4 +11,4 @@ class WordNgramFeatureExtractor(BaseFeatureExtractor):
         # Split text by white space.
         # If you want to extract words from text in more complicated way or using your favorite library like NLTK, please implement in your own.
         words = text.split(self.splitter)
-        return list(map(lambda x: tuple(x), self._each_cons([SENTINAL_CHAR] + words + [SENTINAL_CHAR], self.n)))
+        return self._words_ngram(words, self.n, SENTINAL_CHAR)
