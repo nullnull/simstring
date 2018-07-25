649e100d5d4f91d570b0fceee3257d20cc914160
Katsuma Narisawa <face.the.music721@gmail.com>
2018-07-25 10:22:19 +0900

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="simstring-pure",
-    version="0.0.3",
+    version="1.0.0",
     author="Katsuma Narisawa",
     author_email="katsuma.narisawa@gmail.com",
     description="A Python implementation of the SimString, a simple and efficient algorithm for approximate string matching.",
@@ -14,7 +14,7 @@ setuptools.setup(
     url="https://github.com/nullnull/simstring",
     packages=setuptools.find_packages(),
     classifiers=(
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
