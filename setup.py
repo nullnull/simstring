291d29fb19a02dc0ebf1da73f01d374e69d9352e
Ruben Menke <ruben.m.menke@gmail.com>
2020-12-11 19:22:14 +0100

    upgrade setup

--- setup.py
+++ setup.py
@@ -4,20 +4,26 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="simstring-pure",
-    version="1.0.0",
-    author="Katsuma Narisawa",
-    author_email="katsuma.narisawa@gmail.com",
-    description="A Python implementation of the SimString, a simple and efficient algorithm for approximate string matching.",
+    name="simstring-fast",
+    version="0.0.1",
+    author="Ruben Menke",
+    author_email="ruben.m.menke@gmail.com",
+    description="A fork of the Python implementation of the SimString by (Katsuma Narisawa), a simple and efficient algorithm for approximate string matching.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/nullnull/simstring",
-    packages=setuptools.find_packages(),
+    url="https://github.com/icfly2/simstring-fast",
+    packages=setuptools.find_packages(exclude=("tests",)),
     classifiers=(
         "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ),
+    extras_require = {
+        "mongo" : ["pymongo",],
+        "mecab" : ["MeCab"],
+    }
+
 )
