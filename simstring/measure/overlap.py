87ad4ccdbcdeb5aaf27ce1209c46e17a39a799fc
Ruben Menke <rum@bankingcircle.com>
2022-05-24 16:17:39 +0200

    basic overlap feature implimented

---
+++ simstring/measure/overlap.py
@@ -0,0 +1,23 @@
+import math
+from typing import Iterable
+from .base import BaseMeasure
+
+class OverlapMeasure(BaseMeasure):
+    def __init__(self, db=None, maxsize: int=100) -> None:
+        super().__init__()
+        if db:
+            self.maxsize = db.max_feature_size()
+        else:
+            self.maxsize = maxsize
+
+    def min_feature_size(self, query_size, alpha) -> int:
+        return 1
+
+    def max_feature_size(self, query_size, alpha) -> int:
+        return self.maxsize
+
+    def minimum_common_feature_count(self, query_size: int, y_size: int, alpha: float) -> int:
+        return int(math.ceil(alpha * min(query_size, y_size)))
+
+    def similarity(self, X: Iterable[str], Y: Iterable[str]) -> int:
+        return min(len(set(X)), len(set(Y)))
