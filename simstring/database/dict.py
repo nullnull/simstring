a4a1d1db8695bd94d1ecd5f039bca8ce68d4b528
Ruben Menke <rum@bankingcircle.com>
2022-06-03 11:48:56 +0200

    cleanup of typing hints

--- simstring/database/dict.py
+++ simstring/database/dict.py
@@ -1,16 +1,15 @@
 from collections import defaultdict
 from typing import List, Set, Dict, Type
 from .base import BaseDatabase
-from simstring.feature_extractor.base import BaseFeatureExtractor
 
 def defaultdict_set():
     return defaultdict(set)
 
 class DictDatabase(BaseDatabase):
-    def __init__(self, feature_extractor: Type[BaseFeatureExtractor]):
+    def __init__(self, feature_extractor):
         self.feature_extractor = feature_extractor
         self.strings: List[str] = []
-        self.feature_set_size_to_string_map: Dict[int, Set[str]] = defaultdict(set)
+        self.feature_set_size_to_string_map: Dict[int, Set[str]] = defaultdict(set) # 3.10 and up only
         self.feature_set_size_and_feature_to_string_map: dict = defaultdict(defaultdict_set)
 
     def add(self, string: str):
