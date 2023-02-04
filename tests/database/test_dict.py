0cfe404da00fd217de894509650b1969706e3e0f
Ruben <ruben.m.menke@gmail.com>
2023-02-04 01:48:52 +0100

    complex comparission

--- tests/database/test_dict.py
+++ tests/database/test_dict.py
@@ -45,4 +45,10 @@ class TestDict(TestCase):
             data2 = pickle.load(f)
 
         new = DictDatabase.from_dict(data2)
-        self.assertEqual(self.db , new)
+        # self.assertEqual(self.db , new)
+        self.assertEqual(self.db._min_feature_size , new._min_feature_size)
+        self.assertEqual(self.db._max_feature_size , new._max_feature_size)
+        self.assertEqual(self.db.feature_extractor.__class__  , new.feature_extractor.__class__ )
+        self.assertEqual(self.db.feature_extractor.n  , new.feature_extractor.n )
+        self.assertEqual(self.db.feature_set_size_to_string_map , new.feature_set_size_to_string_map)
+        self.assertEqual(self.db.feature_set_size_and_feature_to_string_map , new.feature_set_size_and_feature_to_string_map)
