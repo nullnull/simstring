b81b3abf42627703c2896275696a77da0bf831d6
Ruben Menke <rum@bankingcircle.com>
2023-08-15 08:59:12 +0200

    made diskcache work, add still too slow

--- dev/company_names.py
+++ dev/company_names.py
@@ -1,56 +1,45 @@
 # coding: utf-8
-
-import os, sys
-
-import numpy as np
-
 from simstring.feature_extractor.character_ngram import CharacterNgramFeatureExtractor
-from simstring.measure.cosine import (
-    CosineMeasure,
-)  # , OverlapMeasure, LeftOverlapMeasure
+from simstring.measure.cosine import CosineMeasure
+from simstring.measure.overlap import OverlapMeasure, LeftOverlapMeasure
 
-# from simstring.database.mongo import MongoDatabase
 from simstring.database.dict import DictDatabase
+from simstring.database.disk import DiskDatabase
 from simstring.searcher import Searcher
+from tqdm import tqdm
 
 from pyinstrument import Profiler
 
 profiler = Profiler()
 
 
-def output_similar_strings_of_each_line(path, measure):
+def output_similar_strings_of_each_line(path, measures, db_cls):
     strings = []
     with open(path, "r") as lines:
         for line in lines:
-            strings.append(line.rstrip("\r\n"))
+            strings.append(line.rstrip("\r\n").strip().lower())
+    
+    db = make_db(db_cls, strings)
 
-    db = DictDatabase(CharacterNgramFeatureExtractor(2))
-    for string in strings:
+    for measure in measures:
+        searcher = Searcher(db, measure)
+        profiler.start()
+
+        for string in strings:
+            result = searcher.search(string, 0.8)
+
+        profiler.stop()
+        print(result)
+        print(db_cls.__name__, measure.__class__.__name__)
+        profiler.print()
+
+def make_db(db_cls, strings):
+    db = db_cls(CharacterNgramFeatureExtractor(2))
+    for string in tqdm(strings):
         db.add(string)
+    return db
 
-    # db.save("companies.db")
-
-    # dbl = DictDatabase.load("companies.db")
-
-    searcher = Searcher(db, measure)
-    profiler.start()
-
-    for string in strings:
-        result = searcher.search(string, 0.8)
-        # result = [str(np.round(x[0], 5)) + ' ' + x[1] for x in searcher.ranked_search(string, 0.8)]
-        # print("\t".join([string, ",".join(result)]))
-
-    profiler.stop()
-    print(result)
-    profiler.print()
-    # profiler.open_in_browser()
-
-
-measure = CosineMeasure()
-output_similar_strings_of_each_line("dev/data/company_names.txt", measure)
-
-# measure = OverlapMeasure()
-# output_similar_strings_of_each_line("dev/data/company_names.txt", measure)
-
-# measure = LeftOverlapMeasure()
-# output_similar_strings_of_each_line("./data/company_names.txt", measure)
+if __name__ =="__main__":
+    measures =  [CosineMeasure(), OverlapMeasure(), LeftOverlapMeasure()]
+    for db_cls in [DictDatabase,DiskDatabase]:
+            output_similar_strings_of_each_line("dev/data/company_names.txt", measures, db_cls)
---
+++ tests/database/test_disk.py
@@ -0,0 +1,63 @@
+# -*- coding:utf-8 -*-
+
+from unittest import TestCase
+from simstring.database.disk import DiskDatabase
+from simstring.feature_extractor.character_ngram import CharacterNgramFeatureExtractor
+import pickle
+import os
+
+
+class TestDict(TestCase):
+    strings = ["a", "ab", "abc", "abcd", "abcde"]
+
+    def setUp(self):
+        self.db = DiskDatabase(CharacterNgramFeatureExtractor(2))
+        for string in self.strings:
+            self.db.add(string)
+
+    def test_strings(self):
+        self.assertEqual(self.db.strings, self.strings)
+
+    # def test_min_feature_size(self):
+    #     self.assertEqual(self.db.min_feature_size(), min(map(lambda x: len(x) + 1, self.strings)))
+
+    # def test_max_feature_size(self):
+    #     self.assertEqual(self.db.max_feature_size(), max(map(lambda x: len(x) + 1, self.strings)))
+
+    def test_lookup_strings_by_feature_set_size_and_feature(self):
+        self.assertEqual(
+            self.db.lookup_strings_by_feature_set_size_and_feature(4, "ab_1"),
+            set(["abc"]),
+        )
+        self.assertEqual(
+            self.db.lookup_strings_by_feature_set_size_and_feature(3, "ab_1"),
+            set(["ab"]),
+        )
+        self.assertEqual(
+            self.db.lookup_strings_by_feature_set_size_and_feature(2, "ab_1"), set([])
+        )
+
+    def test_load_from_folder(self):
+
+        with open("test.pkl", "wb") as f:
+            pickle.dump(self.db, f)
+
+        
+        with open("test.pkl", "rb") as f:
+            new =  pickle.load(f)
+
+        self.assertEqual(self.db._min_feature_size, new._min_feature_size)
+        self.assertEqual(self.db._max_feature_size, new._max_feature_size)
+        self.assertEqual(
+            self.db.feature_extractor.__class__, new.feature_extractor.__class__
+        )
+        self.assertEqual(self.db.feature_extractor.n, new.feature_extractor.n)
+        self.assertEqual(
+            set(self.db.feature_set_size_to_string_map.iterkeys()), set(new.feature_set_size_to_string_map.iterkeys())
+        )
+        self.assertEqual(
+            set(self.db.feature_set_size_and_feature_to_string_map.iterkeys()),
+            set(new.feature_set_size_and_feature_to_string_map.iterkeys()),
+        )
+
+        os.remove("test.pkl")
