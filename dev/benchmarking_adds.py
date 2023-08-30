9e1b4be4bb5fd9d8449e75d2c1e5cd40fc8498df
Ruben Menke <rum@bankingcircle.com>
2023-08-30 22:22:00 +0200

    more testing

--- dev/benchmarking_adds.py
+++ dev/benchmarking_adds.py
@@ -7,6 +7,8 @@ from simstring.database.dict import DictDatabase
 from simstring.database.disk import DiskDatabase
 from simstring.searcher import Searcher
 from tqdm import tqdm
+from time import time
+from multiprocessing import Pool, cpu_count
 
 from pyinstrument import Profiler
 
@@ -20,25 +22,31 @@ def output_similar_strings_of_each_line(path, measures, db_cls):
             strings.append(line.rstrip("\r\n").strip().lower())
     
     db = make_db(db_cls, strings[:10_000])
+    # profiler.print()
 
     for measure in measures:
         searcher = Searcher(db, measure)
-        profiler.start()
 
         for string in strings:
             result = searcher.search(string, 0.8)
 
-        profiler.stop()
         print(result)
         print(db_cls.__name__, measure.__class__.__name__)
-        profiler.print()
 
-    return profiler
 
 def make_db(db_cls, strings):
     db = db_cls(CharacterNgramFeatureExtractor(2))
-    for string in tqdm(strings):
-        db.add(string)
+    # profiler.start()
+    start = time()
+    print('started')
+
+    # for string in tqdm(strings):
+    #     db.add(string)
+    with Pool(8) as p:  
+       p.map(db.add,strings)
+    # profiler.stop()
+    print(time()-start)
+    # profiler.print()
     return db
 
 if __name__ =="__main__":
@@ -47,8 +55,10 @@ if __name__ =="__main__":
     # file = "dev/data/addresses.csv"
     # measures =  [CosineMeasure(), OverlapMeasure(), LeftOverlapMeasure()]
     measures =  [CosineMeasure()]
-    for db_cls in [DictDatabase, DiskDatabase]:
-        profiler = output_similar_strings_of_each_line(file, measures, db_cls)
+    for db_cls in [DiskDatabase]:
+        output_similar_strings_of_each_line(file, measures, db_cls)
 
     # for db_cls in [DictDatabase,DiskDatabase]:
     #         output_similar_strings_of_each_line("dev/data/unabridged_dictionary2.txt", measures, db_cls)
+    # with open("profiler_add.html","w") as file:
+    #     file.write(profiler.output_html())
---
+++ tests/database/test_dbs.py
@@ -0,0 +1,61 @@
+# -*- coding:utf-8 -*-
+
+from unittest import TestCase
+from simstring.database.dict import DictDatabase
+from simstring.database.disk import DiskDatabase
+from simstring.feature_extractor.character_ngram import CharacterNgramFeatureExtractor
+import os
+import shutil
+from multiprocessing import Pool, cpu_count
+
+
+from faker import Faker
+
+
+class TestComparability(TestCase):
+    f = Faker()
+    Faker.seed(0)
+
+    def setUp(self):
+        self.strings = [self.f.name().replace('-',' ') for _ in range(100)]
+
+        self.dict_db = DictDatabase(CharacterNgramFeatureExtractor(2))
+        for string in self.strings:
+            self.dict_db.add(string)
+
+        self.disk_db =  DiskDatabase(CharacterNgramFeatureExtractor(2), path="tmp_db_for_tests")
+        with Pool(8) as p:  
+            p.map(self.disk_db.add, self.strings)
+
+    def tearDown(self) -> None:
+        shutil.rmtree(self.disk_db.path)
+        return super().tearDown()
+    
+    def test_strings(self):
+        self.assertEqual(set(self.dict_db.all()), set(self.disk_db.all()))
+
+    def test_min_feature_size(self):
+        self.assertEqual(self.dict_db.min_feature_size(), self.disk_db.min_feature_size())
+
+    def test_max_feature_size(self):
+        self.assertEqual(self.dict_db.max_feature_size(), self.disk_db.max_feature_size())
+
+    def test_equivalence_disk_to_dict(self):
+        for key in self.disk_db.feature_set_size_to_string_map.iterkeys():
+            self.assertEqual(self.dict_db.feature_set_size_to_string_map[key], self.disk_db.feature_set_size_to_string_map[key])
+
+        for key in self.disk_db.feature_set_size_and_feature_to_string_map.iterkeys():
+            disk_val = self.disk_db.feature_set_size_and_feature_to_string_map[key]
+            k1, k2 = key.split('-')
+            dict_val = self.dict_db.feature_set_size_and_feature_to_string_map[int(k1)][k2]
+            self.assertEqual(disk_val, dict_val)
+
+
+    def test_equivalence_dict_to_disk(self):
+        for size, value in self.dict_db.feature_set_size_and_feature_to_string_map.items():
+            for feature, dict_value in value.items():
+                disk_value = self.disk_db.get_feature_set_size_and_feature_to_string_map(size, feature)
+                self.assertEqual(dict_value, disk_value)
+
+        for size, string_set in self.dict_db.feature_set_size_to_string_map.items():
+            self.assertEqual(string_set, self.disk_db.feature_set_size_to_string_map[size])
